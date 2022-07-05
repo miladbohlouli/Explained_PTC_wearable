c2488302e98098e9a116f5072821aca6b840860f
milad <miladbohlouli@gmail.com>
2022-07-05 12:33:36 +0200

    Interface updated for removing the identifiers bug and redundant features

--- Feature_selectors/interface.py
+++ Feature_selectors/interface.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
+
 class FeatureSelector:
     def __init__(self, include_changing_parameters: bool = False):
-        self.label_title = None
+        self.label_title = "therm_pref"
         self.selected_features_list = []
         self.include_changing_parameters = include_changing_parameters
 
-
+        # Add the unchanging parameters for houses
         if self.include_changing_parameters:
             print("Adding the unchangings")
             self.add_unchanging_parameters()
-
 
     def fit(self, dataset):
         """
@@ -24,7 +24,12 @@ class FeatureSelector:
         """"
         The function for getting selecting the features from a dataset and returning the dataset with selected features
         """
-        raise Exception("not implemented")
+        if self.selected_features_list is []:
+            raise Exception("The feature selector needs to be trained at first")
+
+        if type(dataset) is not pd.DataFrame:
+            dataset = pd.DataFrame(dataset)
+        return dataset[self.selected_features_list], dataset[self.label_title]
 
     def get_feature_size(self):
         if self.selected_features_list is []:
@@ -35,3 +40,21 @@ class FeatureSelector:
     def add_unchanging_parameters(self):
         self.selected_features_list += ['Sex', 'Age', 'Height', 'Weight', 'ColdSens', 'ColdExp', 'Workhr',
                                        'Coffeeintake', 'location']
+
+    def __remove_unchanging(self):
+        ls = ['Sex', 'Age', 'Height', 'Weight', 'ColdSens', 'ColdExp', 'Workhr',
+                                       'Coffeeintake', 'location']
+        for item in ls:
+            if item in self.selected_features_list:
+                self.selected_features_list.remove(item)
+
+    def __remove_uniques(self):
+        for item in ["ID", "Vote_time"]:
+            if item in self.selected_features_list:
+                self.selected_features_list.remove(item)
+
+    def remove_redundant(self):
+        self.__remove_uniques()
+        self.__remove_unchanging()
+        if self.label_title in self.selected_features_list:
+            self.selected_features_list.remove(self.label_title)
