553e86a01604e327a5eb79beba9ef6ca0f3a0417
milad <miladbohlouli@gmail.com>
2022-07-05 12:45:03 +0200

    the training backbone restructured for individual homes

--- trainers/mlp_individual_trainer.py
+++ trainers/mlp_individual_trainer.py
@@ -1,8 +1,9 @@
 from torch.utils.data import DataLoader
 from models.mlp import *
 from torch.utils.tensorboard import SummaryWriter
+from Feature_selectors.threshold_feature_selector import threshold_feature_selector
 from Feature_selectors.simple_feature_selector import simple_feature_selector
-from NAhandlers.averaging import averaging_na_handler
+from NAhandlers.adaptive import adaptive
 from normalizers.guassian_normalizer import guassian_normalizer
 from data.mlp_data_loader import mlp_dataset_individual
 import numpy as np
@@ -10,25 +11,25 @@ import logging
 from torch.nn import CrossEntropyLoss
 from torch.optim import Adam
 import os
-from utils import evaluate_predictions
-from utils import train_test_split
+from utils import evaluate_model, train_test_split, train_step
 import warnings
 warnings.filterwarnings("ignore")
 from ray import tune
-from ray.tune import CLIReporter
-from functools import partial
 
 
 def train(
-        config=None,
-        checkpoint_dir=None,
-        data_dir=None,
-        logging_dir=None,
-        parameter_tuning=False,
-        feature_selector=None,
-        na_handler=None,
-        normalizer=None
+        config = None,
+        checkpoint_dir = None,
+        data_dir = None,
+        logging_dir = None,
+        parameter_tuning = False,
+        feature_selector_type: str = None,
+        na_handler_type: str = None,
+        normalizer_type:str = None
     ):
+    assert feature_selector_type is not None
+    assert na_handler_type is not None
+    assert normalizer_type is not None
 
     # Setting the logging and the other tools for saving
     train_writer = SummaryWriter(os.path.join(os.path.join(logging_dir, "tensorboard"), "train"))
@@ -51,11 +52,15 @@ def train(
     train_dataset_dir = os.path.join(data_dir, "train.csv")
     test_dataset_dir = os.path.join(data_dir, "test.csv")
     house_eval_accuracies = dict()
-    assert feature_selector is not None
-    assert na_handler is not None
-    assert normalizer is not None
+
     for home_id in home_ids:
         house_eval_accuracies[home_id] = []
+        feature_selector, na_handler, normalizer = get_preprocessing_tools(
+            feature_selector_type,
+            na_handler_type,
+            normalizer_type,
+            config
+        )
         train_ds = mlp_dataset_individual(
             path=train_dataset_dir,
             shuffle=True,
@@ -78,7 +83,9 @@ def train(
 
         feature_size = train_ds.feature_selector.get_feature_size()
         model = build_mlp_model(
-            [feature_size] + config["layers"]
+            layers=[feature_size] + config["layers"],
+            activation=config["activation"],
+            dropout=config["dropout"]
         ).float()
 
         logger.info(model)
@@ -92,16 +99,10 @@ def train(
             batch_size=config["batch_size"],
         )
 
-        test_loader = DataLoader(
-            dataset=test_ds,
-            batch_size=config["batch_size"],
-        )
-
         logger.info(f"Training the model for home {home_id} with datasize {len(train_ds)}")
 
         num_epochs = int(config["num_epochs"])
         num_train_samples = len(train_ds)
-        num_test_samples = len(test_ds)
         best_eval_accuracy = 0
         validation_accuracy_not_improved_epochs = 0
 
@@ -109,56 +110,52 @@ def train(
         for epoch in range(num_epochs):
             model.train()
             train_loss_avr = 0
-            eval_loss_avr = 0
             train_acc_avr = 0
             train_balanced_acc_avr = 0
-            eval_acc_avr = 0
             eval_balanced_acc_avr = 0
-            total_conf_matrix = np.zeros((3, 3))
 
             for samples, labels in train_loader:
-                res = model(samples.float())
-                train_loss = loss_model(res, labels)
+                train_results = train_step(
+                    samples=samples,
+                    labels=labels,
+                    model=model,
+                    loss_model=loss_model,
+                    optimizer=optimizer
+                )
 
-                optimizer.zero_grad()
-                train_loss.backward()
-                optimizer.step()
-                global_step += 1
+                train_acc, train_balanced_acc, train_loss = train_results
+                num_samples = len(samples)
 
-                # Calculate the accuracy
-                train_acc, train_balanced_acc, _ = evaluate_predictions(res.detach().numpy(), labels)
-                train_loss_avr += train_loss.detach().numpy() * len(samples)
-                train_acc_avr += train_acc * len(samples)
-                train_balanced_acc_avr += train_balanced_acc * len(samples)
+                train_loss_avr += train_loss
+                train_acc_avr += train_acc * num_samples
+                train_balanced_acc_avr += train_balanced_acc * num_samples
 
                 train_writer.add_scalar(f"home_id: {home_id}/loss", train_loss, global_step)
                 train_writer.add_scalar(f"home_id: {home_id}/accuracy", train_acc, global_step)
                 train_writer.add_scalar(f"home_id: {home_id}/balanced accuracy", train_balanced_acc, global_step)
 
+                global_step += 1
+
             # evaluating the model
             model.eval()
-            for samples, labels in test_loader:
-                res = model(samples.float())
+            eval_acc, eval_balanced_acc, eval_loss = evaluate_model(
+                loss_model=loss_model,
+                model=model,
+                test_ds=test_ds
+            )
 
-                eval_loss = loss_model(res, labels)
-
-                eval_acc, eval_balanced_acc, eval_conf_matrix = evaluate_predictions(res.detach().numpy(), labels)
-                eval_loss_avr += eval_loss.detach().numpy() * len(samples)
-                eval_acc_avr += eval_acc * len(samples)
-                eval_balanced_acc_avr += eval_balanced_acc * len(samples)
-                total_conf_matrix += eval_conf_matrix
-
-                eval_writer.add_scalar(f"home_id: {home_id}/loss", eval_loss, global_step)
-                eval_writer.add_scalar(f"home_id: {home_id}/accuracy", eval_acc, global_step)
-                eval_writer.add_scalar(f"home_id: {home_id}/balanced accuracy", eval_balanced_acc, global_step)
+            eval_writer.add_scalar(f"home_id: {home_id}/loss", eval_loss, global_step)
+            eval_writer.add_scalar(f"home_id: {home_id}/accuracy", eval_acc, global_step)
+            eval_writer.add_scalar(f"home_id: {home_id}/balanced accuracy", eval_balanced_acc, global_step)
 
             logger.info(f"home_id ({home_id})\t"
-                  f"Epoch ({epoch+1:3} / {num_epochs})\t train_loss: {train_loss_avr / num_train_samples:.2f}\t"
-                  f"train_acc (balanced): {train_balanced_acc_avr / num_train_samples:.2f}\t\t"
-                  f"eval_acc (balanced): {eval_balanced_acc_avr / num_test_samples:.2f}\t")
+                        f"Epoch ({epoch+1:3} / {num_epochs})\t train_loss: {train_loss_avr / num_train_samples:.2f}\t"
+                        f"eval_loss: {eval_loss:.2f}\t\t"
+                        f"train_acc (balanced): {train_acc_avr / num_train_samples:.2f}\t\t"
+                        f"eval_acc (balanced): {eval_acc:.2f}\t")
 
-            if eval_balanced_acc_avr / num_test_samples > best_eval_accuracy:
-                best_eval_accuracy = eval_balanced_acc_avr / num_test_samples
+            if eval_balanced_acc_avr > best_eval_accuracy:
+                best_eval_accuracy = eval_balanced_acc_avr
                 validation_accuracy_not_improved_epochs = 0
 
             else:
@@ -168,7 +165,7 @@ def train(
                 logger.info("Early stopping the training due to no change in validation accuracies")
                 break
 
-        house_eval_accuracies[home_id] = eval_balanced_acc_avr / num_test_samples
+        house_eval_accuracies[home_id] = eval_acc
 
     if parameter_tuning:
         tune.report(accuracy=np.average(list(house_eval_accuracies.values())))
@@ -180,28 +177,31 @@ if __name__ == '__main__':
 
     config = dict()
     # model parameters
-    config["layers"] = [128, 3]
+    config["layers"] = [128, 128, 3]
     config["activation"] = "Relu"
+    config["dropout"] = 0.2
+
+    # Data preprocessing parameters
+    feature_selector = "threshold_feature_selector"
+    na_handler = "adaptive"
+    normalizer = "guassian_normalizer"
 
     # Training parameters
     config["batch_size"] = 2
-    config["num_epochs"] = 150
-    config["weight_decay"] = 1e-6
-    config["lr"] = 1e-3
-    config["validation_not_improved"] = 30
+    config["num_epochs"] = 30
+    config["weight_decay"] = 1e-4
+    config["lr"] = 1e-4
+    config["validation_not_improved"] = 10
+    config["threshold"] = 0.05
 
     data_dir = os.path.abspath("../data")
     logging_dir = os.path.abspath("")
 
-    feature_selector = simple_feature_selector()
-    na_handler = averaging_na_handler()
-    normalizer = guassian_normalizer()
-
     train(
         config=config,
-        feature_selector=feature_selector,
-        na_handler=na_handler,
-        normalizer=normalizer,
+        feature_selector_type=feature_selector,
+        na_handler_type=na_handler,
+        normalizer_type=normalizer,
         logging_dir=logging_dir,
         data_dir=data_dir
     )
