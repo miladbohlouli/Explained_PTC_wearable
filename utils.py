08892da652fea44fb855ea003ffbd9dbb9f27d3a
milad <miladbohlouli@gmail.com>
2022-07-05 14:07:52 +0200

    Added the kohen kappa evaluation metric

--- utils.py
+++ utils.py
@@ -1,18 +1,10 @@
-import configparser
 import os.path
-import sys
-from typing import Dict
-import warnings
 import numpy as np
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, confusion_matrix
-import pandas as pd
 from tqdm import tqdm
 import logging
 import pandas as pd
-import pickle as pk
 from scipy.special import softmax
-from sklearn.metrics import roc_auc_score
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import cohen_kappa_score
 from Feature_selectors.simple_feature_selector import simple_feature_selector
 from Feature_selectors.threshold_feature_selector import threshold_feature_selector
@@ -21,6 +13,7 @@ from NAhandlers.averaging import averaging_na_handler
 from NAhandlers.dropping import dropping_handler
 from NAhandlers.adaptive import  adaptive
 from normalizers.guassian_normalizer import guassian_normalizer
+from samplers.SMOTE import SMOTE
 
 
 def train_test_split(path, train_division=0.8):
@@ -59,14 +52,18 @@ def get_available_home_ids(path):
     return sorted(dataset["ID"].unique())
 
 
-def evaluate_predictions(logits, labels):
+def evaluate_predictions(logits, labels, train: bool = True):
     probs = softmax(logits, axis=1)
+    kohen_kappa = None
     binary_labels = np.zeros((len(labels), 3))
     binary_labels[:, labels] = 1
     predicted_labels = np.argmax(logits, 1)
     accuracy = accuracy_score(labels, predicted_labels)
     balanced_accuracy = balanced_accuracy_score(labels, predicted_labels)
-    kohen_kappa = cohen_kappa_score(labels, predicted_labels)
+    if not train:
+        print("predicted:", predicted_labels)
+        print("gt:", labels)
+        kohen_kappa = cohen_kappa_score(labels, predicted_labels)
     return accuracy, balanced_accuracy, kohen_kappa
 
 
@@ -82,8 +79,8 @@ def evaluate_model(loss_model, model, test_ds):
     ds, labels = test_ds.get_tensor_data()
     res = model(ds)
     eval_loss = loss_model(res, labels)
-    eval_acc, eval_balanced_acc, eval_conf_matrix = evaluate_predictions(res.detach().numpy(), labels)
-    return eval_acc, eval_balanced_acc, eval_loss
+    eval_acc, eval_balanced_acc, kohen_kappa = evaluate_predictions(res.detach().numpy(), labels, train=False)
+    return eval_acc, eval_balanced_acc, eval_loss, kohen_kappa
 
 
 def train_step(
@@ -100,7 +97,7 @@ def train_step(
     optimizer.step()
 
     # Calculate the accuracy
-    train_acc, train_balanced_acc, _ = evaluate_predictions(res.detach().numpy(), labels)
+    train_acc, train_balanced_acc, _ = evaluate_predictions(res.detach().numpy(), labels, train=True)
     return train_acc, train_balanced_acc, train_loss.detach().numpy()
 
 
@@ -108,11 +105,13 @@ def get_preprocessing_tools(
         feature_selector: str,
         na_handler: str,
         normlizer: str,
+        resampling: str,
         config
     ):
     fs = None
     na = None
     nor = None
+    res = None
     if feature_selector == "threshold_feature_selector":
         fs = threshold_feature_selector(threshold=config["threshold"])
     elif feature_selector == "simple_feature_selector":
@@ -129,4 +128,7 @@ def get_preprocessing_tools(
 
     if normlizer == "guassian_normalizer":
         nor = guassian_normalizer()
-    return fs, na, nor
+
+    if resampling == "SMOTE":
+        res = SMOTE()
+    return fs, na, nor, res
