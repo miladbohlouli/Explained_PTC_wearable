7072dbe6752ea0776ca01faafb265476cbd0e97e
milad <miladbohlouli@gmail.com>
2022-07-05 20:57:37 +0200

    Tuning the hyper parameters reformed to the new structure

--- experiments/mlp/individual_setting/Test1/Test1_individual.py
+++ experiments/mlp/individual_setting/Test1/Test1_individual.py
@@ -18,25 +18,33 @@ from ray import tune
 from ray.tune import CLIReporter
 from functools import partial
 from trainers.mlp_individual_trainer import train
+import pickle as pk
 
 if __name__ == '__main__':
 
     config = dict()
     # model parameters
     config["layers"] = tune.grid_search([[128, 3], [256, 3], [128, 128, 3], [128, 256, 3], [256, 256, 3], [128, 128, 128, 3]])
-    # config["layers"] = [128, 3]
+    # config["layers"] = [128, 128, 3]
     config["activation"] = "Relu"
+    config["dropout"] = 0.2
+
+    # Data preprocessing parameters
+    config["feature_selector"] = "threshold_feature_selector"
+    config["na_handler"] = "adaptive"
+    config["normalizer"] = "guassian_normalizer"
+    config["resampler"] = "None"
 
     # Training parameters
-    config["batch_size"] = tune.grid_search([2, 8])
-    config["num_epochs"] = 150
-    config["weight_decay"] = tune.grid_search([1e-5, 1e-6])
-    config["lr"] = tune.grid_search([1e-3, 1e-4, 1e-5])
-    config["validation_not_improved"] = 30
+    config["batch_size"] = 16
+    config["num_epochs"] = 30
+    config["weight_decay"] = 1e-4
+    config["lr"] = 1e-4
+    config["validation_not_improved"] = 10
+    config["threshold"] = 0.05
 
     data_dir = os.path.abspath("../../../../data")
     logging_dir = os.path.abspath("")
-    print(logging_dir)
 
     feature_selector = simple_feature_selector()
     na_handler = averaging_na_handler()
@@ -45,27 +53,27 @@ if __name__ == '__main__':
     reporter = CLIReporter(
         metric_columns=["accuracy", "training_iteration"]
     )
-    func = partial(train,
-                   data_dir=data_dir,
-                   logging_dir=logging_dir,
-                   feature_selector=feature_selector,
-                   na_handler=na_handler,
-                   normalizer=normalizer,
-                   parameter_tuning=True)
+    func = partial(
+        train,
+        data_dir=data_dir,
+        logging_dir=logging_dir,
+        parameter_tuning=True
+        )
+
     result = tune.run(
         func,
         resources_per_trial={"cpu": 8},
         config=config,
         metric="accuracy",
         mode="max",
-        progress_reporter=reporter,
-        log_to_file=True
     )
 
     best_result = result.get_best_trial("accuracy", "max", "last")
 
-    print(best_result.config)
-    with open(logging_dir + "/best_config.txt", "wb") as file:
-        file.write(best_result.config)
-
     print(f"accuracy: {best_result.last_result['accuracy']}")
+
+    print(best_result.config)
+    with open(logging_dir + "/best_config.pk", "wb") as file:
+        pk.dump(best_result.config, file)
+
+
--- experiments/mlp/overall_setting/Test1/Test1_overall.py
+++ experiments/mlp/overall_setting/Test1/Test1_overall.py
@@ -4,56 +4,60 @@ from normalizers.guassian_normalizer import guassian_normalizer
 import os
 import warnings
 warnings.filterwarnings("ignore")
+import pickle as pk
 from ray import tune
-from ray.tune import CLIReporter
 from functools import partial
 from trainers.mlp_overall_trainer import train
 
 if __name__ == '__main__':
     config = dict()
     # model parameters
-    config["layers"] = tune.grid_search([[128, 3], [256, 3], [128, 128, 3], [128, 256, 3], [256, 256, 3], [128, 128, 128, 3]])
-    # config["layers"] = [128, 3]
+    config = dict()
+    # model parameters
+    config["layers"] = tune.grid_search(
+        [[128, 3], [256, 3], [128, 128, 3], [128, 256, 3], [256, 256, 3], [128, 128, 128, 3]])
+    # config["layers"] = [128, 128, 3]
     config["activation"] = "Relu"
+    config["dropout"] = 0.2
+
+    # Data preprocessing parameters
+    config["feature_selector"] = "threshold_feature_selector"
+    config["na_handler"] = "adaptive"
+    config["normalizer"] = "guassian_normalizer"
+    config["resampler"] = "None"
 
     # Training parameters
     config["batch_size"] = tune.grid_search([2, 8])
-    config["num_epochs"] = 150
+    config["num_epochs"] = 30
     config["weight_decay"] = tune.grid_search([1e-5, 1e-6])
     config["lr"] = tune.grid_search([1e-3, 1e-4, 1e-5])
-    config["validation_not_improved"] = 30
+    config["validation_not_improved"] = 10
+    config["threshold"] = 0.05
 
-    data_dir = os.path.abspath("../../../data")
+    data_dir = os.path.abspath("../../../../data")
     logging_dir = os.path.abspath("")
-    print(logging_dir)
 
     feature_selector = simple_feature_selector()
     na_handler = averaging_na_handler()
     normalizer = guassian_normalizer()
 
-    reporter = CLIReporter(
-        metric_columns=["accuracy", "training_iteration"]
-    )
     func = partial(train,
                    data_dir=data_dir,
                    logging_dir=logging_dir,
-                   feature_selector=feature_selector,
-                   na_handler=na_handler,
-                   normalizer=normalizer)
+                   parameter_tuning=True
+                   )
     result = tune.run(
         func,
         resources_per_trial={"cpu": 8},
         config=config,
         metric="accuracy",
         mode="max",
-        progress_reporter=reporter,
-        log_to_file=True
     )
 
     best_result = result.get_best_trial("accuracy", "max", "last")
 
     print(best_result.config)
-    with open(logging_dir + "/best_config.txt", "wb") as file:
-        file.write(best_result.config)
-
     print(f"accuracy: {best_result.last_result['accuracy']}")
+
+    with open(logging_dir + "/best_config.pk", "wb") as file:
+        pk.dump(best_result.config, file)
